2e59cdaeb8a286b20570ebbb39624ebf0daa83bd
Jess Robinson <castaway@desert-island.me.uk>
2014-03-05 09:55:48 +0000

    Somewhat scruffy spike to "make table shapes from placeholders" work

--- pptx/shapes/placeholder.py
+++ pptx/shapes/placeholder.py
@@ -7,6 +7,7 @@ Placeholder object.
 from pptx.spec import PH_TYPE_TBL
 from pptx.oxml.graphfrm import CT_GraphicalObjectFrame
 from pptx.oxml.ns import namespaces
+from pptx.oxml.core import SubElement, child, Element
 from pptx.shapes.table import Table
 from pptx.spec import PH_ORIENT_HORZ, PH_SZ_FULL, PH_TYPE_OBJ, PH_TYPE_TBL
 
@@ -64,6 +65,18 @@ class Placeholder(object):
         """Placeholder 'idx' attribute, e.g. '0'"""
         return int(self._ph.get('idx', 0))
 
+    @property
+    def _layout_placeholder(self):
+        """
+        The slide layout placeholder that this placeholder shape inherits from
+        """
+        slide = self._decorated.part
+        slide_layout = slide.slidelayout
+        for ph in slide_layout.shapes.placeholders:
+            if ph.idx == self.idx:
+                return ph
+        raise KeyError['No matching placeholder shape found in slide_layout']
+
     def insert_table(self, rows, cols, left=None, top=None, width=None, height=None):
         """
         Replace an existing Table placeholder shape with an actual table. See also add_table in the ShapeCollection.
@@ -71,15 +84,16 @@ class Placeholder(object):
         if not self.is_table_shape:
             raise TypeError("cannot insert table in a non-table shape/placeholder")
 
+        layout_ph = self._layout_placeholder
         # Use placeholder size and location if not supplied.
         if left == None:
-            left = self.left
+            left = layout_ph.left
         if top == None:
-            top = self.top
+            top = layout_ph.top
         if width == None:
-            width = self.width
+            width = layout_ph.width
         if height == None:
-            height = self.height
+            height = layout_ph.height
 
         shape_idx = self.id - 1
         name = 'Table %d' % (shape_idx)
@@ -90,8 +104,17 @@ class Placeholder(object):
         shapetree = underlying_shape._parent
         # See: http://lxml.de/api/lxml.etree._Element-class.html
         shapetree._spTree.replace(self._element, graphicFrame)
+        ## Make sure we set the related placeholder element in the new GraphicFrame
+        ## This could probably be done more sanely by passing an idx or ph
+        ## to the new_table call.
+        nvPr = Element('p:nvPr')
+        graphicFrame.insert(0, nvPr)
+        ph = Element('p:ph')
+        nvPr.insert(0, ph)
+        ph.set('idx', str(self.idx))
+        ph.set('type', 'tbl')
+
         table = Table(graphicFrame, self._parent)
         this_shape_index = shapetree.index(underlying_shape)
         shapetree._shapes[this_shape_index] = table
         return table
-
